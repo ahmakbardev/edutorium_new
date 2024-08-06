1a21ee6938d7d8ddd5357480f7bb9aaf5bff87ff
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2024-08-06 10:21:47 +0700

    update style

--- resources/views/user/layouts/components/header.blade.php
+++ resources/views/user/layouts/components/header.blade.php
@@ -9,18 +9,18 @@
                         d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
             </a>
-            <div class="ml-3 hidden md:hidden lg:block">
+            {{-- <div class="ml-3 hidden md:hidden lg:block">
                 <!-- form -->
                 <form class="flex items-center">
                     <input type="search"
                         class="border border-gray-300 text-gray-900 rounded focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2 px-3 disabled:opacity-50 disabled:pointer-events-none"
                         placeholder="Search" />
                 </form>
-            </div>
+            </div> --}}
         @endif
         <!-- navbar nav -->
         <ul class="flex ml-auto items-center">
-            <li class="dropdown stopevent mr-2">
+            {{-- <li class="dropdown stopevent mr-2">
                 <a class="text-gray-600" href="#" role="button" id="dropdownNotification"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -87,7 +87,7 @@
                         </div>
                     </div>
                 </div>
-            </li>
+            </li> --}}
             <!-- list -->
             <li class="dropdown ml-2">
                 <a class="rounded-full" href="#" role="button" id="dropdownUser" data-bs-toggle="dropdown"
